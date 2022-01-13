030f0f363e66eeb867d2f008898b901f2b25ebdb
ibrokhim <mr017programmer@gmail.com>
2022-01-13 14:23:18 +0500

    first lesson

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import { Routes, Route, Link } from "react-router-dom";
 import './App.css';
+import Home from "./components/Home";
+import About from "./components/About";
+import Blog from "./components/Blog";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/posts">Blog</Link>
       </header>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/posts" element={<Blog />} />
+        <Route path="*" element={<Blog />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Blog.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BLog = () => {
+  return (
+    <div>
+      <h1>BLog</h1>
+    </div>
+  );
+};
+
+export default BLog;
